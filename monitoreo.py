01c2982b67ed96cf2acf7e2ac826d4f40cc44bb6
Dynamo-arg <72266387+Dynamo-arg@users.noreply.github.com>
2020-11-24 13:30:38 -0300

    Add files via upload

--- monitoreo.py
+++ monitoreo.py
@@ -1,1 +1,22 @@
+#!/usr/bin/env python
+'''
+Proyecto integrador
+Monitoreo de la competencia en Mercado libre
+---------------------------
+Autor: Sebastian Volpe
+Version: 1.00
+Descripcion:
+Programa que se utiliza para monitorear a la competencia en MercadoLibre
+y generar un archivo “csv”. 
+Filtra y analizar la información e informa
+la diferencia de precios entre la Cuenta principal 
+y 3 competidores previamente asignados.
 
+'''
+
+__author__ = "Sebastian Volpe"
+__email__ = "compras@bari.com.ar"
+__version__ = "1.00"
+
+import csv
+import re

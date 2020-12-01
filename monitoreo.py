0663dd15cfc935092793847a8690bc63d845176e
Dynamo-arg <72266387+Dynamo-arg@users.noreply.github.com>
2020-12-01 20:45:02 -0300

    Add files via upload

--- monitoreo.py
+++ monitoreo.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import csv
-import re
 
 '''
 Proyecto integrador
@@ -10,10 +9,10 @@ Autor: Sebastian Volpe
 Version: 1.00
 Descripcion:
 Programa que se utiliza para monitorear a la competencia en MercadoLibre
-y generar un archivo “csv”. 
+y generar un archivo csv 
 Filtra y analizar la información e informa
 la diferencia de precios entre la Cuenta principal 
-y 3 competidores previamente asignados.
+y 3 competidores previamente asignados
 '''
 
 __author__ = "Sebastian Volpe"
@@ -54,14 +53,14 @@ def monitoreo():
 
     # Creo un Bucle para obtener el usuario principal
     # Y lo borro de la lista para continuar
-    bucle = 2
-    while bucle == 2: 
+    bucle = 1
+    while bucle == 1: 
         print(
             "\nListas de usuarios disponibles:\n"
-            "1 - ",usuarios[0],"\n"
-            "2 - ",usuarios[1],"\n"
-            "3 - ",usuarios[2],"\n"
-            "4 - ",usuarios[3],"\n"
+            "1 - ", usuarios[0], "\n"
+            "2 - ", usuarios[1], "\n"
+            "3 - ", usuarios[2], "\n"
+            "4 - ", usuarios[3], "\n"
             "Ingrese el usuario de la cuenta principal:"
             )
         opcion = numero_entero()
@@ -78,9 +77,9 @@ def monitoreo():
         while True:    
             print(
                 "\ningrese que competidor desea monitorear:\n"
-                "1 - ",usuarios[0],"\n"
-                "2 - ",usuarios[1],"\n"
-                "3 - ",usuarios[2],"\n"
+                "1 - ", usuarios[0], "\n"
+                "2 - ", usuarios[1], "\n"
+                "3 - ", usuarios[2], "\n"
                 "4 -  Todos"
                 )
             opcion = numero_entero()
@@ -97,19 +96,20 @@ def monitoreo():
         # Creo un diccionario para organizar y almacenar
 
         comparaciones_salida = {
-            "nombre cuenta":str,
-            "nombre competidor":str,
-            "titulo":str,
-            "precio":int,
-            "diferencia precio":int,
-            "cantidad vendida":int,
-            "diferencia cantidad":int,
+            "nombre cuenta": str,
+            "nombre competidor": str,
+            "titulo": str,
+            "precio": int,
+            "diferencia precio": int,
+            "cantidad vendida": int,
+            "diferencia cantidad": int,
             }
         fo = open('salida.csv', 'w', newline='')
         writer = csv.DictWriter(fo, fieldnames=comparaciones_salida)
         writer.writeheader()
     
         palabra_clave = minusculas()
+        error = 0
         for i in range(cantidad_filas):
             row = data[i]
             titulo = str(row.get("titulo"))
@@ -126,6 +126,9 @@ def monitoreo():
                     })
                 precio_comparar = precio
                 cantidad_comparar = vendidas
+                error += 1
+
+
         # Creo un Try para la comparacion de un solo competidor
         # Si el usuario eligio Todos, no se creo una varibale COMPETIDOR
         # Por lo tanto intenta Except
@@ -158,40 +161,58 @@ def monitoreo():
                 vendidas = int(row.get("cantidad"))
                 if (palabra_clave in titulo) and (cuenta != cuenta_principal):
                         writer.writerow({
-                        "nombre competidor": cuenta,
-                        "titulo": titulo,
-                        "precio": precio,
-                        "cantidad vendida": vendidas,
-                        "diferencia cantidad": vendidas - cantidad_comparar,
-                        "diferencia precio": precio - precio_comparar,
-                        })
+                            "nombre competidor": cuenta,
+                            "titulo": titulo,
+                            "precio": precio,
+                            "cantidad vendida": vendidas,
+                            "diferencia cantidad": vendidas - cantidad_comparar,
+                            "diferencia precio": precio - precio_comparar,
+                            })
             fo.close()
-        print("Archivo generado y Almacenado como ", fo.name)
-        print(
-          "Eliga algunas de estas opciones:\n"
-          "1 - Ver resultados por consola\n"
-          "2 - Volver a generar el Archivo\n"
-          "3 - Salir"
-          )
-        bucle = numero_entero()
-        if bucle == 2:
-            usuarios.append(cuenta_principal)
-        elif bucle == 1:
-            with open('salida.csv') as csvfile:
-                data = list(csv.DictReader(csvfile))
-## ACA            filas = len(data)
-            for i in range(cantidad_filas):
-                row = data[i]
-                print('Cuenta Principal:', row.get('nombre cuenta'))
-                print('Competidor:', row.get('nombre competidor'))
+        if error == 0:
+            print(
+                "NO HAY COINCIDENCIA\n",
+                "1 - Volver a ingresar\n",
+                "2 - SALIR"
+                )
+            bucle = numero_entero()
+            if bucle == 1:
+                usuarios.append(cuenta_principal)
+            else:
+                break
 
-
-
-
-    
-
-        
-
+            
+        else:
+            print("Archivo generado y Almacenado como ", fo.name)
+            print(
+            "Eliga algunas de estas opciones:\n"
+            "1 - Volver a generar el Archivo\n"
+            "2 - Ver resultados por consola\n"
+            "3 - Salir"
+            )
+            bucle = numero_entero()
+            if bucle == 1:
+                usuarios.append(cuenta_principal)
+            elif bucle == 2:
+                with open('salida.csv') as csvfile:
+                    data = list(csv.DictReader(csvfile))
+                filas = len(data)
+                for i in range(filas):
+                    row = data[i]
+                    print(
+                        'Cuenta Principal:', row.get('nombre cuenta'),
+                        'Competidor:', row.get('nombre competidor'),
+                        'Titulo:', row.get('titulo'),
+                        'Precio:', row.get('precio'),
+                        'Diferencia Precio:', row.get('diferencia precio'),
+                        'Vendidas:', row.get('cantidad vendida'),
+                        'Cantidad Vendidas:', row.get('diferencia cantidad')
+                    )
+                    
+            else:
+                break
+                     
+  
 
 if __name__ == '__main__':
     monitoreo()
